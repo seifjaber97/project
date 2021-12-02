bbd27bee0272690746cae0e68eb40aadfd1f83b6
zakatia-charra <you@examplegit config --global user.name Your>
2021-12-02 20:57:18 +0100

    student part updated

--- flask-project/home.py
+++ flask-project/home.py
@@ -1,15 +1,37 @@
 from flask import Flask, render_template, redirect, url_for, request
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
 	return render_template('home.html')
 
+@app.route('/result',methods=['POST','GET'])
+def result_page():
+	file="./all-subjects-prediction.xlsx"
+	d=pd.read_excel(file)
+	if request.method=="POST":
+		ID=request.form['id_std']
+		s1=request.form['s1']
+		s2=request.form['s2']
+		s3=request.form['s3']
+		s4=request.form['s4']
+		s5=request.form['s5']
+		s6='avg rank'
+		s7='Predicted Result'
+		data=d[d['SI_ID']==int(ID)]
+		data=data[[s1,s2,s3,s4,s5,s6,s7]]
+		cols=[s1,s2,s3,s4,s5]
+		d=[data,cols,ID]
+
+	return render_template('result.html',data=d)
+
+	
 @app.route('/staff')
 def staff_page():
 	return render_template('staff.html')
 
-@app.route('/student')
+@app.route('/student',methods=['GET','POST'])
 def student_page():
 	return render_template('/student.html')		
 
--- flask-project/student.py
+++ flask-project/student.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/student')
+@app.route('/student', methods=['GET','POST'])
 def staff_page():
 	return render_template('student.html')
-
+"""
+@app.route('/result',methods=['POST','GET'])
+def result_page():
+	if request.method=="POST":
+		ID=request.from['id_std']
+	return render_template('result.html',data=ID)
+"""
 if __name__ == '__main__':
 	app.debug = True
 	app.run()	
